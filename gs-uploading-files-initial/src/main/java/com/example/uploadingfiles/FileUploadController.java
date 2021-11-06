a845e77bda276b1478588c59dfe99370fe5fed18
cventimiglia <79486361+cventimiglia@users.noreply.github.com>
2021-11-06 19:59:35 -0400

    fixes the format with the test case file

--- gs-uploading-files-initial/src/main/java/com/example/uploadingfiles/FileUploadController.java
+++ gs-uploading-files-initial/src/main/java/com/example/uploadingfiles/FileUploadController.java
@@ -34,12 +34,17 @@ import com.example.uploadingfiles.fileParsing.Parameter;
 import com.example.uploadingfiles.storage.StorageFileNotFoundException;
 import com.example.uploadingfiles.storage.StorageService;
 
+
+//This file was imported using spring boot. We did not create this file, but simply modified it to fit our needs
+//link to the source: https://spring.io/guides/gs/uploading-files/
+//This class is used to decide routes for the API to take.
 @Controller
 public class FileUploadController {
 
 	private final StorageService storageService;
-	private final FileParser fileParser;
+	private final FileParser fileParser; // add this instance variable in order to use our custom methods
 
+	//update the constructor to use our instance variable
 	@Autowired
 	public FileUploadController(StorageService storageService, FileParser fileParser) {
 		this.storageService = storageService;
@@ -47,6 +52,7 @@ public class FileUploadController {
 	}
 	
 
+	//This code remained unchanged. It is used to render our view when the page is loaded.
 	@GetMapping("/")
 	public String listUploadedFiles(Model model) throws IOException {
 
@@ -59,6 +65,7 @@ public class FileUploadController {
 	}
 
 
+	//This code remained unchanged. This is to get a specific file resource stored in our upload-dir folder. 
 	@GetMapping("/files/{filename:.+}")
 	@ResponseBody
 	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
@@ -68,23 +75,32 @@ public class FileUploadController {
 	}
 	
 
+	//This method was modified in order to gain the functionality we need.
+	//The purpose of this method is to store the JSON file submitted in order to use it
+	//to parse and create combinations. Then it will write to a new file and add that to the
+	//upload-dir folder. 
 	@PostMapping("/")
 	public String handleFileUpload(@RequestParam("file") MultipartFile file,
 			RedirectAttributes redirectAttributes) {
 		
+		//create the text file name based on the JSON file
 		String fileName = file.getOriginalFilename();
 		int dotIndex = fileName.lastIndexOf(".");
 		String fileNameNoExt = fileName.substring(0, dotIndex);
 
 		try {
+			// store the json file in order to use it later
 			storageService.store(file);			
 			
+			//create the File object inside of the upload-dir location. Create the BufferedWriter to write to the File
 			File outputFile = new File("upload-dir/" + fileNameNoExt + "-combos.txt");
 			BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
 	
+			//parse the file and create the count variable and update it as you populate the arrayList
 			ArrayList<Parameter> arrList = fileParser.parseFile("upload-dir/" + file.getOriginalFilename());
 			int count = 1;
 
+			//write to the file and update the count variable so it can be used later.
 			for (Parameter temp : arrList) {
 				writer.write("Parameter Name: " + temp.getName() + " | Equivalence Classes: " + temp.getEquivalenceClasses());
 				writer.write("\n");
@@ -93,33 +109,36 @@ public class FileUploadController {
 			
 			writer.write("\n");
 	
+			//create a matrix and populate it using the createCombos method. Pass in arrList and count as parameters
 			String[][] combos = fileParser.createCombos(arrList, count);
 			
-			/*
-			for (int i = 0; i < arrList.size(); i++) {
-				writer.write("\t" + arrList.get(i).getName());
-			}
-			writer.write("\n");
-			*/
+			//this is used to write the test case combinations to the text file.
 			int testCaseNumber = 1;
 			for (int row = 0; row < combos.length; row++) {
-				writer.write("Test Case " + testCaseNumber + ": ");
+				if (testCaseNumber < 10) {
+					writer.write("Test Case " + testCaseNumber + ":   ");
+				} else if (testCaseNumber >= 10 && testCaseNumber < 100) {
+					writer.write("Test Case " + testCaseNumber + ":  ");
+				} else { 
+					writer.write("Test Case " + testCaseNumber + ": ");
+				}
 				testCaseNumber++;
 				for (int column = 0; column < combos[row].length; column++) {
-						writer.write(combos[row][column] + " ");
+					writer.write(combos[row][column] + " ");
 				}
-	
 				writer.write("\n");
 			}
 	
 			writer.close();
 		
+		//catch and error and throw an error message. This gets rid of the server error page
 		} catch (Exception e) {
 			//e.printStackTrace();
 			redirectAttributes.addFlashAttribute("message", e.getMessage());
 			return "redirect:/";
 		}
 
+		//return a message to indicate the file was successfully submitted and that they recieved their text file.
 		redirectAttributes.addFlashAttribute("message",
 				"You successfully uploaded " + file.getOriginalFilename() +"\n"
 						+ "and recieved " + fileNameNoExt + "-combos.txt!");
